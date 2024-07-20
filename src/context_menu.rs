77e30d3d2534d8325f47467799ceb1a306c4b16c
Beaverr <theboxguy659@gmail.com>
2024-07-20 16:32:42 -0500

    Initial commit

---
+++ src/context_menu.rs
@@ -0,0 +1,24 @@
+use crate::{stt, Context, Error};
+use poise::serenity_prelude as serenity;
+use std::path::Path;
+
+/// Query information about a Discord profile
+#[poise::command(context_menu_command = "Transcribe audio in message", slash_command)]
+pub async fn transcribe(
+    ctx: Context<'_>,
+    #[description = "Voice Message to transcribe"] msg: serenity::Message,
+) -> Result<(), Error> {
+    if msg.attachments.is_empty() {
+        ctx.say("No message attachments found. :p").await?;
+        return Ok(());
+    }
+
+    // Is ./voice-message atm bc it is in relation to the binary
+    // Not the source file
+    // I am smort
+    let path = Path::new("./voice-message.ogg");
+
+    ctx.say(stt::transcribe(path)).await?;
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,56 @@
+mod context_menu;
+mod stt;
+
+use dotenv;
+use poise::serenity_prelude as serenity;
+
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+pub struct Data {}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![
+                context_menu::transcribe()
+            ],
+            on_error: |error| {
+                Box::pin(async move {
+                    println!("what the hell");
+                    match error {
+                        poise::FrameworkError::ArgumentParse { error, .. } => {
+                            if let Some(error) = error.downcast_ref::<serenity::RoleParseError>() {
+                                println!("Found a RoleParseError: {:?}", error);
+                            } else {
+                                println!("Not a RoleParseError :(");
+                            }
+                        }
+                        other => poise::builtins::on_error(other).await.unwrap(),
+                    }
+                })
+            },
+            ..Default::default()
+        })
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let token = std::env::var("DISCORD_TOKEN").unwrap();
+    let intents =
+        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+
+    client.unwrap().start().await.unwrap()
+}
